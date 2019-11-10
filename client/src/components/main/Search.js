0ed095a8ea70d309fe48941ff4ad44fb571de86b
John Steen <john@johnsteen.co>
2019-11-09 19:23:54 -0600

    Add search component

---
+++ client/src/components/main/Search.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { withApollo } from 'react-apollo';
+import gql from 'graphql-tag';
+// import CoffeeShop from './CoffeeShop';
+// import Coffee from './Coffee';
+
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      coffeeShops {
+        id
+        name
+        founded
+        address {
+          street
+          city
+          state
+          zip
+        }
+        type
+        baristaSatisfaction
+      }
+    }
+  }
+`
+
+class Search extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      shops: [],
+      filter: ''
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Search
+          <input
+            type='text'
+            onChange={e => this.setState({ filter: e.target.value })}
+          />
+          <button onClick={() => this._executeSearch()}>
+            OK
+          </button>
+        </div>
+        {/* Uncomment when component is built */}
+        {/* {this.state.shops.map((shop, idx) => (
+          <CoffeeShop key={shop.id} shop={shop} index={idx} />
+        ))} */}
+      </div>
+    )
+  }
+
+  _executeSearch = async () => {
+
+  }
+}
+
+export default withApollo(Search);
