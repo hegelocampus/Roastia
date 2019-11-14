05ba4854b90722de24644b93b890d3b63e0a90df
Bee <pizzaman8099@gmail.com>
2019-11-13 18:04:21 -0800

    Add working blur screen
    Need to add bigger version of the image before pushing to production

--- client/src/components/main/Splash.js
+++ client/src/components/main/Splash.js
@@ -4,10 +4,14 @@ import "./splash.scss";
 
 export default () => (
   <React.Fragment>
-    <section className="splash-main">
-      <div className="splash-inner">
-        <h1>Discover a coffee shop where you'll love to code</h1>
-        <Search />
+    <section className="splash-main-container">
+      <div className="splash-backdrop"></div>
+      <div className="splash-main">
+        <div className="splash-backdrop-blur" />
+        <div className="splash-inner">
+          <h1>Discover a coffee shop where you'll love to code</h1>
+          <Search />
+        </div>
       </div>
     </section>
   </React.Fragment>
