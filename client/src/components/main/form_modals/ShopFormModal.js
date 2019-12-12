b2c2ba385f82453dbf5b907754fdd8f6c0a14656
Bee <pizzaman8099@gmail.com>
2019-12-12 14:26:52 -0800

    Finish fixing form modals

--- client/src/components/main/form_modals/ShopFormModal.js
+++ client/src/components/main/form_modals/ShopFormModal.js
@@ -2,13 +2,16 @@ import React, { useRef } from 'react';
 import { useHistory, useLocation } from 'react-router-dom';
 import CoffeeShopForm from '../coffee_shop/CoffeeShopForm';
 import useOnOutsideClick from '../../util/useOnOutsideClick';
+import usePreventScrollWhenOpen from '../../util/usePreventScrollWhenOpen';
 import './ShopFormModal.scss';
 
 export default ({ background }) => {
   const history = useHistory();
   const location = useLocation();
   const formType = location.pathname.match(/([^/]+)\/?$/)[0];
-  const shop = location.state.shop;
+  const shop = location.state.shop || null;
+
+  usePreventScrollWhenOpen();
 
   const modalContent = useRef(null);
   useOnOutsideClick(modalContent, e => {
@@ -18,7 +21,7 @@ export default ({ background }) => {
 
   return (
     <div className="shop-modal-screen">
-      <div className="-shop-modal-content" ref={modalContent}>
+      <div className="shop-modal-content" ref={modalContent}>
         <div className="shop-modal-body">
           <CoffeeShopForm shop={shop} formType={formType} />
         </div>
