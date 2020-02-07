29685b41c0aea2b1e8b7e89b8ff7b9dcc259c3c3
Bee <pizzaman8099@gmail.com>
2020-02-07 13:09:40 -0800

    Add basic search tests

--- cypress/integration/search_spec.js
+++ cypress/integration/search_spec.js
@@ -9,9 +9,25 @@ describe('Search Spec', () => {
       .should('have.value', 'Seattle');
   });
 
-  it('Search bar renders coffees', () => {
+  //FIXME: Need to tell cypress to similate hovering over element so this works
+  //consistently
+  it('Search bar renders coffee shops by city', () => {
     cy.get('.react-autosuggest__input')
       .type('Seattle')
-      .should('have.value', 'Seattle');
+    cy.contains('Slate Coffee');
+  });
+
+  //FIXME: Need to tell cypress to similate hovering over element so this works
+  //consistently
+  it('Search bar renders coffees by orgin', () => {
+    cy.get('.react-autosuggest__input')
+      .type('Guatemala');
+    cy.contains('Gateway');
+  });
+
+  it('Search bar renders coffees by roast', () => {
+    cy.get('.react-autosuggest__input')
+      .type('Guatemala');
+    cy.contains('Gateway');
   });
 });
