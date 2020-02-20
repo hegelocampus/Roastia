4bc59355b78f77d71c9ca32f7113a2853e601976
Bee <pizzaman8099@gmail.com>
2020-02-20 09:54:20 -0800

    Start refactoring of User model

--- packages/server/src/models/User.js
+++ packages/server/src/models/User.js
@@ -2,53 +2,48 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        min: 6,
-        max: 32
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    },
-    favorites: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "coffeeShops"
-        }
-    ]
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 32
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  favorites: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "coffeeShops"
+    }
+  ],
 });
 
-UserSchema.statics.addFavorite = (userId, coffeeShopId) => {
-    const User = mongoose.model("users");
+UserSchema.statics.addFavorite = async function(userId, coffeeShopId) {
+  const CoffeeShop = mongoose.model("coffeeShops");
+
+  user = await this.findById(userId);
+  shop = await CoffeeShop.findById(coffeeShopId);
+  user.favorites.push(shop);
+  shop.users.push(user);
+  return Promise.all([user.save(), shop.save()]).then(
+      ([user, shop]) => user
+  );
+}
+
+UserSchema.statics.removeFavorite = async function(userId, coffeeShopId) {
     const CoffeeShop = mongoose.model("coffeeShops");
 
-    return User.findById(userId).then(user => {
-        return CoffeeShop.findById(coffeeShopId).then(shop => {
-            user.favorites.push(shop);
-            shop.users.push(user);
-
-            return Promise.all([user.save(), shop.save()]).then(
-                ([user, shop]) => user
-            );
-        });
-    });
-};
-
-UserSchema.statics.removeFavorite = (userId, coffeeShopId) => {
-    const User = mongoose.model("users");
-    const CoffeeShop = mongoose.model("coffeeShops");
-
-    return User.findById(userId).then(user => {
+    return this.findById(userId).then(user => {
         return CoffeeShop.findById(coffeeShopId).then(shop => {
             user.favorites.pull(shop);
             shop.users.pull(user);
