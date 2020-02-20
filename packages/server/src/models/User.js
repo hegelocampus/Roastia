dee8676a6cac78255a20445648b452d6111c1860
Bee <pizzaman8099@gmail.com>
2020-02-20 14:42:34 -0800

    Refactor add/RemoveShopTo/FromFav

--- packages/server/src/models/User.js
+++ packages/server/src/models/User.js
@@ -28,28 +28,35 @@ const UserSchema = new Schema({
   ],
 });
 
-const findUserAndShopById = async (userId, shopId) => {
-  const User = mongoose.model("users");
-  const CoffeeShop = mongoose.model("coffeeShops");
+const findUserAndShop = async (token, shopId) => {
+  const AuthService = require("../services/auth");
+  const { id: userId, loggedIn } = await AuthService.verifyUser({ token });
 
-  const user = await User.findById(userId);
-  const shop = await CoffeeShop.findById(shopId);
-  return { user, shop };
+  if (loggedIn) {
+    const User = mongoose.model("users");
+    const CoffeeShop = mongoose.model("coffeeShops");
+
+    const user = await User.findById(userId);
+    const shop = await CoffeeShop.findById(shopId);
+    return { user, shop };
+  } else {
+    throw "Active user not found";
+  }
 }
 
 const saveUserAndShop = async (user, shop) => {
   try {
     await user.save();
     await shop.save();
-    return { user, shop };
+    return user;
   } catch(e) {
     console.error(e);
     return -1;
   }
 }
 
-UserSchema.statics.addFavorite = async function(userId, coffeeShopId) {
-  const { user, shop } = await findUserAndShopById(userId, coffeeShopId);
+UserSchema.statics.addFavorite = async function(token, coffeeShopId) {
+  const { user, shop } = await findUserAndShop(token, coffeeShopId);
 
   user.favorites.push(shop);
   shop.users.push(user);
@@ -57,8 +64,8 @@ UserSchema.statics.addFavorite = async function(userId, coffeeShopId) {
   return saveUserAndShop(user, shop);
 }
 
-UserSchema.statics.removeFavorite = async function(userId, coffeeShopId) {
-  const { user, shop } = await findUserAndShopById(userId, coffeeShopId);
+UserSchema.statics.removeFavorite = async function(token, coffeeShopId) {
+  const { user, shop } = await findUserAndShop(token, coffeeShopId);
 
   user.favorites.pull(shop);
   shop.users.pull(user);
